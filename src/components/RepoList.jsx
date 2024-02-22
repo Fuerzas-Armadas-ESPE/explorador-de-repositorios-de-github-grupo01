292c87b74bd1ab4b5e78935da6fbc03c6c906fb1
mvespin1 <107557153+mvespin1@users.noreply.github.com>
2024-02-22 18:57:34 -0500

    Logica Paginacion - ResporList.jsx - por Julaian Escobar

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 
-const RepoList = ({ username }) => {
+const RepoList = ({ username, currentPage, perPage }) => {
   const [repos, setRepos] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,32 +14,51 @@ const RepoList = ({ username }) => {
           `https://api.github.com/users/${username}/repos`
         );
         const sortedRepos = response.data.sort((a, b) => b.size - a.size);
-        const topRepos = sortedRepos.slice(0, 5);
-        setRepos(topRepos);
+        setTotalPages(Math.ceil(sortedRepos.length / perPage));
+
+        // Calcular los índices de inicio y fin para la página actual
+        const startIndex = (currentPage - 1) * perPage;
+        const endIndex = startIndex + perPage;
+
+        const reposForPage = sortedRepos.slice(startIndex, endIndex);
+        setRepos(reposForPage);
       } catch (error) {
         console.error("Error fetching repos:", error);
       }
     };
 
     fetchData();
-  }, [username]);
+  }, [username, currentPage, perPage]);
 
   return (
     <div>
-      <h2>Top 5 repositorios con más participación de {username}</h2>
-      <ul>
-        {repos.map((repo) => (
-          <li key={repo.id}>
-            {repo.name} - Tamaño: {repo.size}
-          </li>
-        ))}
-      </ul>
+      <h2>Top Repositorios de {username}</h2>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nombre del Repositorio</TableCell>
+              <TableCell>Tamaño</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {repos.map((repo) => (
+              <TableRow key={repo.id}>
+                <TableCell>{repo.name}</TableCell>
+                <TableCell>{repo.size}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
 
 RepoList.propTypes = {
   username: PropTypes.string.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  perPage: PropTypes.number.isRequired,
 };
 
 export default RepoList;
