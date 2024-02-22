1d35d786990b004d6f717133d9dbb9d4f3f834e6
mvespin1 <107557153+mvespin1@users.noreply.github.com>
2024-02-22 18:10:55 -0500

    Cambios en interfaz del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
   Container,
   AppBar,
   Toolbar,
+  Paper,
 } from "@mui/material";
 import RepoList from "./components/RepoList";
 
@@ -24,27 +25,44 @@ function App() {
 
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Mi Explorador de Repositorios
+      <AppBar position="static" sx={{ backgroundColor: "#ce6b5d" }}>
+        <Toolbar sx={{ justifyContent: "center" }}>
+          <Typography variant="h6" component="div" sx={{ color: "#FFF" }}>
+            Mi Explorador de Repositorios - Escobar Juliana - Espin Marco
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container maxWidth="sm" sx={{ marginTop: "20px" }}>
-        <form onSubmit={handleFormSubmit}>
-          <TextField
-            label="Nombre de Usuario"
-            variant="outlined"
-            value={username}
-            onChange={handleUsernameChange}
-            fullWidth
-            sx={{ marginBottom: "10px" }}
-          />
-          <Button type="submit" variant="contained" fullWidth>
+      <Container component="main" maxWidth="xs" sx={{ marginTop: "50px" }}>
+        <Paper elevation={3} sx={{ padding: "20px", display: "flex", flexDirection: "column", alignItems: "center", backgroundColor: "#7b9971" }}>
+          <Typography component="h1" variant="h5" sx={{ marginBottom: "20px", color: "#fff" }}>
             Buscar Repositorios
-          </Button>
-        </form>
+          </Typography>
+          <form onSubmit={handleFormSubmit} sx={{ width: "100%" }}>
+            <TextField
+              label="Nombre de Usuario"
+              variant="outlined"
+              value={username}
+              onChange={handleUsernameChange}
+              fullWidth
+              sx={{ marginBottom: "20px", backgroundColor: "#ffefb9" }}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              fullWidth
+              sx={{
+                backgroundColor: "#34502b",
+                color: "#FFF",
+                "&:hover": {
+                  backgroundColor: "#ffefb9", // Cambia a tu color deseado al pasar el ratón
+                  color: "black"
+                },
+              }}
+            >
+              Buscar Repositorios
+            </Button>
+          </form>
+        </Paper>
         {showRepoList && <RepoList username={username} />}
       </Container>
     </div>
